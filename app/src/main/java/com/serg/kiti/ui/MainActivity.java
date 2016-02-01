6150a005e59bf4c3118ed48ab005e78cee3e9a27
Serg285 <Sergei_1979@bk.ru>
2016-02-01 15:28:32 +0400

    изменил структуру

--- app/src/main/java/com/serg/kiti/ui/MainActivity.java
+++ app/src/main/java/com/serg/kiti/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.serg.kiti;
+package com.serg.kiti.ui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.serg.kiti.R;
 
 public class MainActivity extends AppCompatActivity {
 
